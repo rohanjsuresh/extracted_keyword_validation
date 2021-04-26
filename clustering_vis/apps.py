4b33d1e8ca0f87ec2fbf19be20da599fd3c2eb8e
rohansuresh <rohan.suresh12@gmail.com>
2021-04-26 15:47:18 -0500

    Buildingon clustering tool to add dendogram

---
+++ clustering_vis/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ClusteringVisConfig(AppConfig):
+    name = 'clustering_vis'
--- extracted_keyword_validation/urls.py
+++ extracted_keyword_validation/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path(r'', include('keyword_relation.urls')),
     path(r'domainness_tool/', include('domainness_tool.urls')),
     path(r'definition_tool/', include('definition_tool.urls')),
+     path(r'clustering_vis/', include('clustering_vis.urls')),
     path('admin/', admin.site.urls),
     url(r"^accounts/", include("django.contrib.auth.urls")),
     path(r"users/", include('users.urls')),
